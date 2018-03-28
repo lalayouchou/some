5bea04e7f0458a81ebd78530bbfe7f146641fbdc
lalayouchou <1436714481@qq.com>
2018-03-28 16:51:59 +0800

    轮播图的修改与优化

--- AcFun/carousel.js
+++ AcFun/carousel.js
@@ -1,4 +1,4 @@
-(function() {
+(function($) {
 
 function carousel(data) {
 	// 保存this
@@ -20,22 +20,24 @@ function carousel(data) {
 	this.anim();
 
 	// 添加事件，小按钮的点击事件，窗口的移入移出事件，鼠标在窗口内部，停止轮播，鼠标在窗口外部，继续从当前位置开始轮播
-	var A;//这里A一定要设置在外面，不然if语句判断的时候A还未定义
+	var A=null;//这里A一定要设置在外面，不然if语句判断的时候A还未定义
+	// 这里A在构造函数执行的时候，事件处理程序中有了A的引用，所以每次执行的时候，可以调用A，实际事件本身的作用域是和绑定的元素有关系
 	$('div.carousel').on('mouseenter', function(event) {
 		_this_.on=false;
 	});
 
 	$('div.carousel').on('mouseleave', function(event) {
 		// 防抖技术，如果列表中还有定时器，则去除，保留最后一个
+
 		if(A){
 			clearTimeout(A);
-			console.log("清除");
+			console.log(this);//这里的this指的是$('div.carousel')
 		}
 
 		A=setTimeout(function(){
 				_this_.on=true;
 				_this_.anim();
-
+				console.log(this);//windows
 			A=null;	
 				},3000);
 
@@ -43,21 +45,19 @@ function carousel(data) {
 
 	$('div.carousel-nav').on('click', function(event) {
 		if(event.target.nodeName.toLowerCase()==="span"){
-			_this_.index=$(event.target).index();//获取点击的具体节点
-			
-			_this_.on=true;//开启开关，这里因为触发了mouseenter事件，默认是关闭的；
-			// 防止多次点击，防抖
 			if(A){
 				clearTimeout(A);
-				console.log("清除");
+				console.log(this);//这里的this指的是$('div.carousel')
 			}
 
 			A=setTimeout(function(){
+				_this_.index=$(event.target).index();//获取点击的具体节点
+				_this_.on=true;//开启开关，这里因为触发了mouseenter事件，默认是关闭的；
 				_this_.anim();
-				// 执行完毕，初始化
+				_this_.on=false;//这里执行完点击事件后，就把动画函数给关闭，只执行一次
 				A=null;	
-				// _this_.on=false;//这里执行完点击事件后，就把动画函数给关闭，只执行一次
-					},0);//给个0,消除延迟
+					},300);//太低执行不过来，300比较合适
+
 		}
 	});
 
@@ -120,4 +120,4 @@ carousel.prototype={
 
 
  window.carousel=carousel;
-})();
+})(jQuery);
